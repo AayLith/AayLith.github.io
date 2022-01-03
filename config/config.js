2b630f0f0e328702221385176fac820fe3bf4de4
AayLith <bzh-artikodin@hotmail.fr>
2022-01-03 20:48:02 +0100

    3

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (5c4179ba)";
+Config.version = "0.11.2";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',
